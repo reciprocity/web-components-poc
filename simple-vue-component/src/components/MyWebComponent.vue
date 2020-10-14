1896a884b5b35504196a7a0b65509ad0f9c2b515
Jan Savli <jan.savli@gmail.com>
2020-10-14 10:40:58 +0200

    Style simple web component

--- simple-vue-component/src/components/MyWebComponent.vue
+++ simple-vue-component/src/components/MyWebComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h1>My Vue Web Component</h1>
-    <div>{{ msg }}</div>
+  <div class="my-web-component">
+    <h1>Simple Vue WC</h1>
+    <div>Dynamic: {{ msg }}</div>
   </div>
 </template>
 
@@ -11,3 +11,21 @@
     props: ['msg']
   }
 </script>
+<style lang="scss" scoped>
+.my-web-component {
+  display: block;
+  max-width: 480px;
+  margin: 1rem auto;
+  background-image: linear-gradient(40deg,#e5e0f0,#fbfaff);
+  padding: 1rem 3rem;
+  padding-top: 0.5rem;
+  border-radius: 3px;
+  box-shadow: 1px 1px 2px rgb(0 0 0 / 14%),
+    1px 3px 11px rgb(0 0 0 / 14%);
+}
+
+h1 {
+  color: #988fb2;
+  margin: 0.1em 0;
+}
+</style>
